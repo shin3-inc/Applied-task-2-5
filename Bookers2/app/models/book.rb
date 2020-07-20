8a1e6fff6cdc7520eb0b09f804783a486536667d
梅澤晴恵 <harueumezawa@HaruenoMacBook-Air.local>
2020-07-20 10:50:51 +0900

    applied task 1 finish

--- Bookers2/app/models/book.rb
+++
@@ -1,8 +0,0 @@
-class Book < ApplicationRecord
-
-  belongs_to :user, optional: true
-
-   validates :title, presence: true ,allow_nil: true
-   validates :body, presence: true, uniqueness: true, length: { maximum: 200 } ,allow_nil: true
-
-end
