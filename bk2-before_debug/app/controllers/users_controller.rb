0cb1913e91100353d45a4cba7fc2201a293d0668
梅澤晴恵 <harueumezawa@HaruenoMacBook-Air.local>
2020-07-24 00:33:38 +0900

    Applied task 4

--- bk2-before_debug/app/controllers/users_controller.rb
+++ bk2-before_debug/app/controllers/users_controller.rb
@@ -35,6 +35,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def followers
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
--- bk2-before_debug/config/routes.rb
+++ bk2-before_debug/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users,only: [:show,:index,:edit,:update] do
     member do
       get :follow
+      get :followers
     end
   end
 
